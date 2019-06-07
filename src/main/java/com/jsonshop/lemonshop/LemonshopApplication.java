f2fa420fca4d72bdb31ba5a43e747d444efab3ab
JasonsSun <1659108996@qq.com>
2019-06-07 11:29:44 +0800

    柠檬项目整合GitHub 2019-6-7

---
+++ src/main/java/com/jsonshop/lemonshop/LemonshopApplication.java
@@ -0,0 +1,13 @@
+package com.jsonshop.lemonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsonshop/lemonshop/LemonshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jsonshop.lemonshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LemonshopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
